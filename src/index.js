7c9ef794f32b2ac42620af288991b721a1cc602c
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2018-02-12 14:23:14 +0000

    Add TODO

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import './index.css';
 // Game
 
 function Square (props) {
+  // TODO: Make the merge of classNames nicer, see egghead.io course.
   return (
     <div className={`square ${props.colorClass || ''}`}></div>
   );
