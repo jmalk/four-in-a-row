fb539206a62800c43a181abdd1f0813d6dc44dd0
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2020-07-24 17:18:02 +0100

    Start TDD-ing check for winner

--- src/__tests__/check-for-winner-test.js
+++ src/__tests__/check-for-winner-test.js
@@ -1,5 +1,63 @@
 import checkForWinner from '../check-for-winner';
 
-it('Exists', () => {
-  expect(checkForWinner()).toEqual(undefined);
+const emptyBoard = Array(7).fill(Array(6).fill(null));
+
+test('should return false for an empty board', () => {
+  expect(checkForWinner(emptyBoard)).toBe(false);
+});
+
+const onePiece = [
+  ["red", null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+];
+
+test('should return false for one piece', () => {
+  expect(checkForWinner(onePiece)).toBe(false);
+});
+
+const fourRedInAColumn = [
+  ["red", "red", "red", "red", null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+];
+
+test('should return true for four red in one column', () => {
+  expect(checkForWinner(fourRedInAColumn)).toBe(true);
+});
+
+const fourBlueInAColumn = [
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  ["blue", "blue", "blue", "blue", null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+];
+
+test('should return true for four blue in one column', () => {
+  expect(checkForWinner(fourBlueInAColumn)).toBe(true);
+});
+
+const fourBlueInAColumnFurtherUp = [
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  ["red", "red", "blue", "blue", "blue", "blue"],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+];
+
+test('should return true for four blue in one column even if not at the bottom', () => {
+  expect(checkForWinner(fourBlueInAColumnFurtherUp)).toBe(true);
 });
--- src/check-for-winner.js
+++ src/check-for-winner.js
@@ -1,3 +1,26 @@
 export default function checkForWinner (boardSnapshot) {
-  console.log(boardSnapshot);
+  const columnHasFourMatching = boardSnapshot.map((column) => {
+    for (let i=0; i<3; i++) {
+      if (
+        (
+          column[i] === 'red' &&
+          column[i] === column[i] &&
+          column[i] === column[i + 1] &&
+          column[i] === column[i + 2] &&
+          column[i] === column[i + 3]
+        ) || (
+          column[i] === 'blue' &&
+          column[i] === column[i] &&
+          column[i] === column[i + 1] &&
+          column[i] === column[i + 2] &&
+          column[i] === column[i + 3]
+        )
+      ) {
+        return true;
+      }
+    }
+    return false;
+  })
+  const aColumnHasFourMatching = columnHasFourMatching.some(Boolean);
+  return aColumnHasFourMatching;
 }
