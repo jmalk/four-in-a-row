c75cd593e1924506dd9f099ad7d407a68abeb15b
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2020-07-24 22:19:21 +0100

    Make column check similar to row check

--- src/check-for-winner.js
+++ src/check-for-winner.js
@@ -1,16 +1,6 @@
 export default function checkForWinner (boardSnapshot) {
-  const columnHasFourMatching = boardSnapshot.map((column) => {
-    for (let i=0; i<3; i++) {
-      const sum = column[i] + column[i + 1] + column[i + 2] + column[i + 3];
-      if (sum === 4 || sum === -4) {
-        return true;
-      }
-    }
-    return false;
-  })
-  const aColumnHasFourMatching = columnHasFourMatching.some(Boolean);
-
   let aRowHasFourMatching = false;
+  let aColumnHasFourMatching = false;
 
   for (let column = 0; column < boardSnapshot.length - 3; column++) {
     for (let row = 0; row < boardSnapshot[column].length - 3; row++) {
@@ -23,6 +13,17 @@ export default function checkForWinner (boardSnapshot) {
 
       if (rowSum === 4 || rowSum === -4) {
         aRowHasFourMatching = true;
+      }
+
+      const columnSum = (
+        boardSnapshot[column][row] +
+        boardSnapshot[column][row + 1] +
+        boardSnapshot[column][row + 2] +
+        boardSnapshot[column][row + 3]
+      );
+
+      if (columnSum === 4 || columnSum === -4) {
+        aColumnHasFourMatching = true;
       }
     }
   }
